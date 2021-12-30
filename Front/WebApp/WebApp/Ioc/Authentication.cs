e264f644996c93ba646aeba95d1df5c247c7ac5b
gabe <gabrielleraujo@gmail.com>
2021-12-30 16:11:10 -0300

    Add Authentication class

---
+++ Front/WebApp/WebApp/Ioc/Authentication.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp.Ioc
+{
+    public static class Authentication
+    {
+        public static void AddCookieAuthentication(this IServiceCollection services)
+        {
+            services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Authentication/Login";
+                    x.LogoutPath = "/Authentication/Logout";
+                });
+        }
+    }
+}
